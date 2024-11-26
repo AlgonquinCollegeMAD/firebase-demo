4a924c7f362cf70a60ed417148654811f280fdfe
Vladimir Cezar <vladimircezar@icloud.com>
2024-11-26 14:41:46 -0500

    Initial Commit

---
+++ Firebase_Demo/Firebase_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_DemoApp.swift
+//  Firebase_Demo
+//
+//  Created by Vladimir Cezar on 2024.11.26.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
